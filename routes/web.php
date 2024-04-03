4f9e86218a09edb973b81799087306dfeeda05f0
Ajung007 <agungrachmadprihanto@gmail.com>
2024-04-03 14:01:42 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -21,23 +21,28 @@ Route::get('/', function () {
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('dashboard')->middleware('auth')->group(function(){
     Route::get('/', [DashboardController::class, 'index'])->name('dash.index');
+    
+    // Pengajuan dan Detail
     Route::get('/pengajuan', [DashboardController::class, 'pengajuan'])->name('dash.pengajuan');
     Route::get('/pengajuan/{id}', [DashboardController::class, 'show'])->name('dash.show');
+    
+    // Download Word
     Route::get('/word/{id}', [DashboardController::class, 'downloadWord'])->name('dash.download');
 
+    // Download Laporan Excel
     Route::get('/laporan', [DashboardController::class, 'laporan'])->name('dash.laporan');
     Route::get('/downloadExcel', [DashboardController::class, 'downloadReport'])->name('dash.download');
     
+    // Generate LINK berdasarkan user
     Route::get('/generate', [DashboardController::class, 'generete'])->name('dash.generate');
 
 });
 
 
-
+// Pengajuan untuk nasabah
 Route::get('/pengajuan/{nip}/{nama}/{no_hp}', [PengajuanController::class, 'index'])->name('pengajuan.index');
 Route::post('/pengajuan', [PengajuanController::class, 'post'])->name('pengajuan.post');
 
